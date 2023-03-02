227d1572d17ab607bc92cf309473ef3ed9a49523
GitHub Action <action@github.com>
2023-03-02 17:14:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/aymanosman/intellijtest6/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/aymanosman/intellijtest6/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.aymanosman.intellijtest6.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aymanosman.intellijtest6.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/aymanosman/intellijtest6/services/MyProjectService.kt
+++ src/main/kotlin/com/github/aymanosman/intellijtest6/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.aymanosman.intellijtest6.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aymanosman.intellijtest6.MyBundle
 
 class MyProjectService(project: Project) {
 
